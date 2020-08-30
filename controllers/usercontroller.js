07952fd88d04fd15eecc829252f343c260c42abb
Kevin Curnow <kevinscurnow@gmail.com>
2020-08-30 13:15:33 -0400

    completed user login route

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -21,4 +21,29 @@ router.post('/register', function(req, res) {
     .catch(err => res.status(500).json({error: err}))
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({ where: {username: req.body.username}})
+    .then(
+        user => {
+            if(user){
+                bcrypt.compare(req.body.password, user.passwordhash, (err, matches) => {
+                    if(matches){
+                        let token = jwt.sign({id: user.id, username: user.username},process.env.JWT_SECRET, {expiresIn: '1d'})
+                        res.json({
+                            user: user,
+                            message: `Successfully authenticated ${user.username}`,
+                            sessionToken: token
+                        })
+                    } else {
+                        res.status(502).send({ error: 'bad gateway'})
+                    }
+                })
+            } else {
+                res.status(500).send({error: `A user with the name ${user.username} does not exist`})
+            }
+        },
+        err => res.status(501).send({error: 'failed to process'})
+    )
+})
+
 module.exports = router;
