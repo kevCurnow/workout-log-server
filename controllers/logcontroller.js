174a45644294fb1e3480d593ca74e7c3cdeaf011
Kevin Curnow <kevinscurnow@gmail.com>
2020-08-30 13:56:24 -0400

    added log get route

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -14,4 +14,13 @@ router.post('/', validateSession, (req, res) => {
     .catch(err => res.status(500).json({error: err}))
 });
 
+router.get('/', validateSession, (req, res) => {
+    let userid = req.user.id
+    Log.findAll({
+        where: { owner_id: userid}
+    })
+    .then(logs => res.status(200).json(logs))
+    .catch(err => res.status(500).json({error: err}))
+})
+
 module.exports = router;
